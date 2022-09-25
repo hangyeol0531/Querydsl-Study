d69733f43f6758da67b6e7e5e5e2a2a7030f26dc
hangyeol0531 <jhan0531@naver.com>
2022-09-26 01:08:20 +0900

    example domain

---
+++ src/main/java/stduy/querysdl/entity/Member.java
@@ -0,0 +1,42 @@
+package stduy.querysdl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = { "id", "username" })
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String useranme;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team){
+        this.useranme = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }
+    }
+
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
