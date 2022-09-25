e341245dfe859677fe1de0e2021f27b09d720de9
hangyeol0531 <jhan0531@naver.com>
2022-09-26 00:12:35 +0900

    Querydsl setting

---
+++ src/test/java/stduy/querysdl/QuerysdlApplicationTests.java
@@ -0,0 +1,39 @@
+package stduy.querysdl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import stduy.querysdl.entity.Hello;
+import stduy.querysdl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerysdlApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+	@Test
+	void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
